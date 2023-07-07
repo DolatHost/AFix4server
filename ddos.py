2a55771cf3e3192e5bc03735a9d0dbc00e501561
DolatHost <138880207+DolatHost@users.noreply.github.com>
2023-07-07 15:58:22 +0330

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,23 @@
+import time
+import subprocess
+
+while True:
+
+    output = subprocess.check_output('sudo ufw status', shell=True).decode()
+    output_lines = output.splitlines()
+
+    ip_packets = []
+    for line in output_lines:
+        if 'Anywhere' in line:
+            parts = line.split(' ')
+            ip = parts[1]
+            if parts[3].isdigit():
+                packets = int(parts[3])
+                ip_packets.append((ip, packets))
+
+    for ip, packets in ip_packets:
+        if packets > 2000:
+            subprocess.run(f'sudo ufw delete allow from {ip}', shell=True)
+
+    time.sleep(300)
+    print("Script Is Running...")
